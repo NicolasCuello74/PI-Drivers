84e78c16f6fcd8e73e9133c01e3d2ca450a1242d
NicolasCuello74 <nicocuello7@gmail.com>
2024-12-18 22:13:03 -0300

    backend-prueba

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const { PORT } = process.env;
-const port = PORT
 
 conn.sync({ force: false }).then(() => {
-server.listen(port, () => {
+server.listen(3001, () => {
   console.log(`Server listening on port`);
 })
 }).catch(error => console.error(error))
